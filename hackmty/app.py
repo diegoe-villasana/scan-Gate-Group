fe565b305a1166f6501173231d6a52c49f7aae2a
diegoe-villasana <diegoe.villasana@gmail.com>
2025-10-25 21:25:29 -0600

    Update app.py

--- hackmty/app.py
+++ hackmty/app.py
@@ -38,5 +38,9 @@ def home():
     
     return render_template("index.html", report=report)
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
